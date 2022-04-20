2b7d258a2022104f255302bf3a67098da37d51f9
Tibedo <thibaudbaily@hotmail.com>
2022-04-20 10:33:58 +0200

    create html css

---
+++ script.js
@@ -0,0 +1,23 @@
+const collection = [
+    {
+      name: "Pikachu",
+      Drawer: "blabla",
+      releaseYear: 1994,
+      blocFrom: "Ex Emerald",
+      picture: "pikachu.jpeg",
+      rarity: ["Ultra rare", "Foil"],
+      languages: [
+        "French",
+        "English",
+        "Japanese",
+        "Italian",
+      ],
+    },
+  
+    
+
+
+
+
+
+  ];
